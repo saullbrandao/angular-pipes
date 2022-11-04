9031497135538a1766cd11c32066d262ad9c13c9
Saull Brandão <saull@outlook.com>
2022-11-04 10:48:02 -0300

    aplicando internacionalizacao nos pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import '@angular/common/locales/global/pt';
 
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 import { CamelCasePipe } from './camel-case.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ExemplosPipesComponent,
-    CamelCasePipe
+  declarations: [AppComponent, ExemplosPipesComponent, CamelCasePipe],
+  imports: [BrowserModule],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
