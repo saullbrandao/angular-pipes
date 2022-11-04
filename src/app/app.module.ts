bb97b5273d1db1c148fd84f068b98b69f09a0efd
Saull Brandão <saull@outlook.com>
2022-11-04 10:33:01 -0300

    using pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExemplosPipesComponent
   ],
   imports: [
     BrowserModule
